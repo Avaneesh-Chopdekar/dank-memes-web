862ec3a106dafe05c6a471fb6e0c4a1ac4791248
Avaneesh-Chopdekar <89315115+Avaneesh-Chopdekar@users.noreply.github.com>
2022-08-20 01:13:49 +0530

    Made App.jsx more readable

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import Box from "@suid/material/Box";
-import AppBar from "@suid/material/AppBar";
-import Toolbar from "@suid/material/Toolbar";
-import Typography from "@suid/material/Typography";
 import Stack from "@suid/material/Stack";
-import Button from "@suid/material/Button";
 
-import ArrowForward from "@suid/icons-material/ArrowForward";
-import Send from "@suid/icons-material/Send";
+import { createEffect, createSignal } from "solid-js";
 
-import { createEffect, createSignal, Show, Suspense } from "solid-js";
+import Navbar from "./components/Navbar";
+import Subreddit from "./components/Subreddit";
+import Meme from "./components/Meme";
+import Controls from "./components/Controls";
 
 import { createTheme, ThemeProvider } from "@suid/material/styles";
-import CircularProgress from "@suid/material/CircularProgress";
 
 const theme = createTheme({
   palette: {
@@ -41,53 +38,11 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Box flexGrow={1}>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography
-              variant="h6"
-              component="h1"
-              flexGrow={1}
-              textAlign="center"
-            >
-              Dank Memes
-            </Typography>
-          </Toolbar>
-        </AppBar>
+        <Navbar />
         <Stack mt={3} gap={2} alignItems="center">
-          <Stack direction="row" alignItems="center">
-            <Typography variant="h6">From r/{subreddit()}</Typography>
-          </Stack>
-          {loaded() ? null : (
-            <div className="progress-indicator-height">
-              <CircularProgress />
-            </div>
-          )}
-          <img
-            src={image()}
-            alt="meme"
-            onLoad={() => setLoaded(true)}
-            style={loaded() ? {} : { display: "none" }}
-          />
-          <Stack
-            m={2}
-            gap={2}
-            direction="row"
-            flexGrow={1}
-            justifyContent="center"
-          >
-            <Button
-              variant="contained"
-              endIcon={<ArrowForward />}
-              onClick={fetchData}
-            >
-              Next
-            </Button>
-            <a href={image()} target="_blank">
-              <Button variant="contained" endIcon={<Send />}>
-                Share
-              </Button>
-            </a>
-          </Stack>
+          <Subreddit subreddit={subreddit} />
+          <Meme image={image} loaded={loaded} setLoaded={setLoaded} />
+          <Controls image={image} fetchData={fetchData} />
         </Stack>
       </Box>
     </ThemeProvider>
