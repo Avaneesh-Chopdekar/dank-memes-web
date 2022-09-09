ab69a2c7f8a914a3aa702fee001fa1466d4e8b1e
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2022-09-10 01:15:53 +0530

    Added theme code in App component

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,22 @@ import Controls from "./components/Controls";
 import useMediaQuery from "@suid/material/useMediaQuery";
 import { createTheme, ThemeProvider } from "@suid/material/styles";
 
-const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-
-const theme = createTheme({
-  palette: {
-    mode: prefersDarkMode ? "dark" : "light",
-    primary: {
-      main: "#FF4500",
-    },
-  },
-});
-
 const [image, setImage] = createSignal("");
 const [subreddit, setSubreddit] = createSignal("");
 const [loaded, setLoaded] = createSignal(false);
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = createTheme({
+    palette: {
+      mode: prefersDarkMode() ? "dark" : "light",
+      primary: {
+        main: "#FF4500",
+      },
+    },
+  });
+
   function fetchData() {
     setLoaded(false);
     fetch("https://meme-api.herokuapp.com/gimme")
